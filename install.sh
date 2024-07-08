7799f7998cce43d0b9cec89d6daaa727ba250542
Mahdi Zafari <163778240+World-Rage-company@users.noreply.github.com>
2024-07-08 16:45:33 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -39,14 +39,6 @@ check_xpanel_installed() {
     echo -e "${RED}XPanel installation directory not found. Please install XPanel and try again.${NC}"
     exit 1
   fi
-
-#  db_exists=$(mysql -e "SHOW DATABASES LIKE 'XPanel_plus'" | grep xpanel_plus)
-#  if [ -z "$db_exists" ]; then
-#    echo -e "${RED}MySQL database 'XPanel_plus' not found. Please configure XPanel correctly.${NC}"
-#    exit 1
-#  else
-#    echo -e "${GREEN}XPanel database 'XPanel_plus' found.${NC}"
-#  fi
 }
 
 configure_needrestart() {
