ebcb573ab429a14e4597697d6715993df0db40ac
Mahdi Zafari <163778240+World-Rage-company@users.noreply.github.com>
2024-07-08 14:20:42 +0330

    update

--- Web_XDashboard/index.php
+++ Web_XDashboard/index.php
@@ -154,12 +154,15 @@ require "assets/php/dashboard_handler.php";
 <section class="training">
     <div class="text" data-key="training">Training</div>
     <div class="training-content">
-        <p>Training.</p>
+        <p>Coming Soon.</p>
     </div>
 </section>
 
 <section class="support">
     <div class="text" data-key="support">Support</div>
+    <div class="support-content">
+        <p>Coming Soon.</p>
+    </div>
 </section>
 
 <script src="assets/js/script.js"></script>
