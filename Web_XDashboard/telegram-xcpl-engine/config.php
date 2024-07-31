cf6da1d586871209891c2f58528cc69061d39dee
Mahdi Zafari <143317931+Mahdi-Zafari@users.noreply.github.com>
2024-08-01 01:07:46 +0330

    Update config.php

--- Web_XDashboard/telegram-xcpl-engine/config.php
+++ Web_XDashboard/telegram-xcpl-engine/config.php
@@ -1,1 +1,10 @@
 <?php
+define('TELEGRAM_BOT_TOKEN', '{BOT_TOKEN_HERE}');
+
+define('BOT_OWNER_ID', '{USER_ID_HERE}');
+
+define('WEBHOOK_URL', '{{protocol}://ip/domain:{port}}/telegram-xcpl-engine/xcpl_bot.php');
+
+define('TELEGRAM_API_URL', 'https://api.telegram.org/bot' . TELEGRAM_BOT_TOKEN);
+
+define('DEBUG_MODE', true);
