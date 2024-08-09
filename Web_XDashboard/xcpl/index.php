618005c7bbf4f99a018ddb04149a3d440ae8c864
World Rage <163778240+World-Rage-company@users.noreply.github.com>
2024-08-09 15:47:11 +0330

    Update index.php

--- Web_XDashboard/xcpl/index.php
+++ Web_XDashboard/xcpl/index.php
@@ -10,7 +10,7 @@ require "assets/php/control_panel_handler.php";
     <title>XCPL</title>
     <link rel="icon" href="../assets/images/xlogo.png" type="image/x-icon">
     <link rel="stylesheet" href="assets/css/style.css">
-    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
 
@@ -19,71 +19,59 @@ require "assets/php/control_panel_handler.php";
     <div id="success-message" class="success-message"></div>
 </div>
 
-<div class="container">
-    <div class="header-container">
-        <div class="header-content">
-            <div class="title">XCPL</div>
-            <div class="bottom-content">
-                <li>
-                    <a href="javascript:void(0)" class="logout-link">
-                        <i class='bx bx-log-out icon'></i>
-                        <span class="text nav-text" data-key="logout">Logout</span>
-                    </a>
-                </li>
-
-                <li class="mode">
-                    <div class="sun-moon">
-                        <i class='bx bx-moon icon moon'></i>
-                        <i class='bx bx-sun icon sun'></i>
-                    </div>
-                    <span class="mode-text text">Dark mode</span>
-                    <div class="toggle-switch-mode">
-                        <span class="switch-mode"></span>
-                    </div>
-                </li>
+<div class="sidebar">
+    <div class="logo-details">
+        <i class='bx bxl-xing icon'></i>
+        <div class="logo_name">XCPL</div>
+        <i class="bx bx-menu" id="btn-sidebar"></i>
+    </div>
+    <ul class="nav-list">
+        <li>
+            <a href="javascript:void(0)">
+                <i class="bx bx-grid-alt"></i>
+                <span class="links_name">Dashboard</span>
+            </a>
+            <span class="tooltip">Dashboard</span>
+        </li>
+        <li>
+            <a href="javascript:void(0)">
+                <i class="bx bx-user"></i>
+                <span class="links_name">User</span>
+            </a>
+            <span class="tooltip">User</span>
+        </li>
+        <!--<li>
+            <a href="#">
+                <i class="bx bx-cart-alt"></i>
+                <span class="links_name">Order</span>
+            </a>
+            <span class="tooltip">Order</span>
+        </li>-->
+        <li>
+            <a href="javascript:void(0)">
+                <i class="bx bx-cog"></i>
+                <span class="links_name">Setting</span>
+            </a>
+            <span class="tooltip">Setting</span>
+        </li>
+        <li class="profile">
+            <div class="profile-details">
+                <img src="../assets/images/xlogo.png" alt="profileImg" />
+                <div class="name_job">
+                    <div class="name"><?php echo $admin_data['username'];?></div>
+                    <div class="Status"><?php echo $admin_data['status'];?></div>
+                </div>
             </div>
-        </div>
-    </div>
-    <div class="setting-container">
-        <div class="setting-header"><i class='bx bx-cog icon'></i>Setting</div>
-        <div class="setting-content">Coming Soon.</div>
-    </div>
-    <div class="Support-container">
-        <div class="Support-header"><i class='bx bx-support icon'></i>Support</div>
-        <div class="Support-content">
-            <div class="ticket-content">Coming Soon.</div>
-        </div>
-    </div>
-    <div class="users-container">
-        <div class="users-header"><i class='bx bx-user icon'></i>Users</div>
-        <div class="users-content">
-            <div class="table__body">
-                <table>
-                    <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>USERNAME</th>
-                        <th>ACCESS</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php foreach ($users as $user): ?>
-                        <tr>
-                            <td><?php echo $user['id']; ?></td>
-                            <td><?php echo $user['username']; ?></td>
-                            <td>
-                                <div class="toggle-switch <?php echo $user['access'] ? 'on' : ''; ?>" data-userid="<?php echo $user['id']; ?>">
-                                    <span class="switch"></span>
-                                </div>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
+            <i class="bx bx-log-out" id="log_out"></i>
+        </li>
+    </ul>
 </div>
+
+<section class="dashboard-section"></section>
+
+<section class="user-section"></section>
+
+<section class="setting-section"></section>
 
 <script src="assets/js/jquery.min.js"></script>
 <script src="assets/js/script.js"></script>
